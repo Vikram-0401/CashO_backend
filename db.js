09e32ab5094d61ab51dbdc777504b55df6a0c87e
Vikram-0401 <vikramshettyr4@gmail.com>
2025-01-24 22:37:44 +0530

    Middleware added

--- db.js
+++ db.js
@@ -1,45 +1,54 @@
-const mongoose = require("mongoose");
-const { string } = require("zod");
+// backend/db.js
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/Paytm");
+mongoose.connect("mongodb://localhost:27017/paytm")
 
-const userSchema = mongoose.Schema({
-    username : {
-        type : string,
-        required : true,
-        unique : true,
-        trim : true,
-        lowercase : true,
-        minLength : 3,
-        maxLength : 30
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
     },
-    
-    password : {
-        type : string,
-        required : true,
-        minLength : 6
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
     },
-
-    firstName : {
-        type : string,
-        required : true,
-        trim : true,
-        maxLength : 50
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
     },
-
-    lastName : {
-        type : string,
-        required : true,
-        trim : true,
-        maxLength : 50
-
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
     }
 });
 
-const user = mongoose.model("User", userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User
-}
-
-
+	User,
+    Account
+};
